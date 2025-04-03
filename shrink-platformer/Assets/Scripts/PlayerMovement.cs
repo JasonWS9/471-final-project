f941726c5be09838f9db2caf1d169a83facb44f3
JasonWS9 <JasonWScott10@gmail.com>
2025-04-03 17:34:35 -0400

    a

--- shrink-platformer/Assets/Scripts/PlayerMovement.cs
+++ shrink-platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,29 +4,72 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-   
+    PlayerInput playerInput;
     CharacterController characterController;
 
 
 
     private Vector2 movement;
-    [SerializeField] private float playerSpeed = 5f;
+    private float playerSpeed;
+    [SerializeField] private float defaultPlayerSpeed = 5f;
+    [SerializeField] private float sprintingPlayerSpeed = 5f;
     [SerializeField] private float rotationSpeed = 5f;
 
     [SerializeField] private Transform cameraTransform;
-    
+
+    private bool isSprinting;
+
+    bool isJumpPressed = false;
+
+    private void OnEnable()
+    {
+        playerInput.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Player.Disable();
+    }
+
     void Awake()
     {
+        playerInput = new PlayerInput();
         characterController = GetComponent<CharacterController>();
+
+        playerInput.Player.Jump.started += OnJump;
+        playerInput.Player.Jump.canceled += OnJump;
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movement();
+        Sprinting();
+    }
 
+
+
+    void OnMove(InputValue moveVal)
+    {
+        movement = moveVal.Get<Vector2>();
+    }
+
+    void OnJump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
+        Debug.Log(isJumpPressed);
+    }
+
+    void handleJump()
+    {
+
+    }
+
+    private void Movement()
+    {
         float MoveX = movement.x;
         float MoveZ = movement.y;
-
 
 
         Vector3 cameraForward = cameraTransform.forward.normalized;
@@ -34,7 +77,6 @@ public class PlayerMovement : MonoBehaviour
         cameraForward.y = 0; cameraRight.y = 0;
 
         Vector3 ActualMovement = (cameraForward * MoveZ) + (cameraRight * MoveX);
-
 
         if (ActualMovement.magnitude > 0.01f)
         {
@@ -44,12 +86,18 @@ public class PlayerMovement : MonoBehaviour
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
 
         }
-
     }
 
-
-    void OnMove(InputValue moveVal)
+    private void Sprinting()
     {
-        movement = moveVal.Get<Vector2>();
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            playerSpeed = sprintingPlayerSpeed;
+        } else
+        {
+            playerSpeed = defaultPlayerSpeed;
+        }
+        
     }
 }
